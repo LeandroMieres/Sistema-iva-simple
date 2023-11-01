e76719ac3f230f6ce6501908abb7fbdfc0089de6
Leandro Mieres <112925301+LeandroMieres@users.noreply.github.com>
2023-11-01 16:05:44 -0300

    Update FacturasAltas.php

--- FacturasAltas.php
+++ FacturasAltas.php
@@ -1,49 +1,53 @@
 <?php
+$conexion = new mysqli("localhost", "root", "", "sistemaiva");
 
-include("config/conexion.php");
-
-$opcion = $_POST["opcion"];
-
-$sql = "INSERT INTO Ventas (Comprobante,Tipo_Comprobante) VALUES ($opcion)";
-$stmt = $conexion->prepare($query);
-$stmt->execute();
-
-$Comprobante = $_POST['Comprobante'];
-$Tipo_Comprobante = $_POST['Tipo_Comprobante'];
-$Numero = $_POST['Numero'];
-$Fecha = $_POST['Fecha'];
-$Id_Contribuyente = $_POST['Id_Contribuyente'];
-$Neto_21 = $_POST['Neto_21'];
-$Neto_10 = $_POST['Neto_10'];
-$Neto_27 = $_POST['Neto_27'];
-$Iva_21 = $_POST['Iva_21'];
-$Iva_10 = $_POST['Iva_10'];
-$Iva_27 = $_POST['Iva_27'];
-$Exento = $_POST['Exento'];
-$No_Grabado = $_POST['No_Grabado'];
-$Impuestos = $_POST['Impuestos'];
-$Percepciones = $_POST['Percepciones'];
-$Retenciones = $_POST['Retenciones'];
-$Gastos = $_POST['Gastos'];
-$Total = $_POST['Total'];
-$Periodo = $_POST['Periodo'];
-$Signo = $_POST['Signo'];
-$id_usuario= $_POST['id_usuario'];
-
-
-$sql = "INSERT INTO ventas (Comprobante, , Numero, Fecha, Id_Contribuyente, Neto_21, Neto_10, Neto_27, Iva_21, Iva_10, Iva_27, Exento, No_Gravado, Impuestos, Percepciones, Retenciones, Gastos, Total, Periodo, Signo, id_usuario)
-        VALUES ($Comprobante,$Tipo_Comprobante,$Numero,$Fecha,$Id_Contribuyente,$Neto_21,$Neto_10,$Neto_27,$Iva_21,$Iva_10,$Iva_27,$Exento,$No_Grabado,$Impuestos,$Percepciones,$Retenciones,$Gastos,$Total,$Periodo,$Signo, $id_usuario)";
-$resultado = $conexion->query($sql);
-if ($resultado->num_rows > 0) {
-    $resultados = $resultado->fetch_assoc();
-
-} else {
-
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
 }
 
-  
+$Comprobante = $_POST['Comprobante'] ?? null;
+$Tipo_Comprobante = $_POST['Tipo_Comprobante'] ?? null;
+$Numero = $_POST['Numero'] ?? null;
+$Fecha = $_POST['Fecha'] ?? null;
+$Id_Contribuyente = $_POST['Id_Contribuyente'] ?? null;
+$Neto_21 = $_POST['Neto_21'] ?? null;
+$Neto_10_5 = $_POST['Neto_10'] ?? null;
+$Neto_27 = $_POST['Neto_27'] ?? null;
+$Iva_21 = $_POST['Iva_21'] ?? null;
+$Iva_10_5 = $_POST['Iva_10'] ?? null;
+$Iva_27 = $_POST['Iva_27'] ?? null;
+$Exento = $_POST['Exento'] ?? null;
+$No_Gravado = $_POST['No_Gravado'] ?? null;
+$Impuestos = $_POST['Impuestos'] ?? null;
+$Percepciones = $_POST['Percepciones'] ?? null;
+$Retenciones = $_POST['Retenciones'] ?? null;
+$Gastos = $_POST['Gastos'] ?? null;
+$Total = $_POST['Total'] ?? null;
+$Periodo = $_POST['Periodo'] ?? null;
+$Signo = $_POST['Signo'] ?? null;
+$id_usuario = $_POST['id_usuario'] ?? null;
 
+print_r($_POST);
 
+if ($Comprobante !== null && $Tipo_Comprobante !== null && $Numero !== null && $Fecha !== null && $Id_Contribuyente !== null && $Neto_21 !== null && $Neto_10_5 !== null && $Neto_27 !== null && $Iva_21 !== null && $Iva_10_5 !== null && $Iva_27 !== null && $Exento !== null && $No_Gravado !== null && $Impuestos !== null && $Percepciones !== null && $Retenciones !== null && $Gastos !== null && $Total !== null && $Periodo !== null && $Signo !== null && $id_usuario !== null) {
+    $sql = "INSERT INTO ventas (Comprobante, Tipo_Comprobante, Numero, Fecha, Id_Contribuyente, Neto_21, Neto_10, Neto_27, Iva_21, Iva_10, Iva_27, Exento, No_Gravado, Impuestos, Percepciones, Retenciones, Gastos, Total, Periodo, Signo, id_usuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $stmt = $conexion->prepare($sql);
 
+    if ($stmt) {
+        $stmt->bind_param("ssssdddddssssssssssss", $Comprobante, $Tipo_Comprobante, $Numero, $Fecha, $Id_Contribuyente, $Neto_21, $Neto_10_5, $Neto_27, $Iva_21, $Iva_10_5, $Iva_27, $Exento, $No_Gravado, $Impuestos, $Percepciones, $Retenciones, $Gastos, $Total, $Periodo, $Signo, $id_usuario);
 
+        if ($stmt->execute()) {
+            echo "Inserción exitosa.";
+        } else {
+            echo "Error en la inserción: " . $stmt->error;
+        }
+        $stmt->close();
+    } else {
+        echo "Error en la preparación de la consulta: " . $conexion->error;
+    }
+
+    $conexion->close();
+} else {
+    echo "Faltan datos obligatorios para la inserción.";
+}
 ?>
