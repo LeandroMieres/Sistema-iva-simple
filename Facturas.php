11ee13b783f0721878a75ebc336ac9ad24924d50
Leandro Mieres <112925301+LeandroMieres@users.noreply.github.com>
2023-11-01 16:04:37 -0300

    Update Facturas.php

--- Facturas.php
+++ Facturas.php
@@ -1,117 +1,75 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="View/Css/ventas.css"> -->
-    <title>Formulario</title>
+    <title>Facturas Ventas</title>
 </head>
 <body>
-    <div class="form-container">
-        <form action="FacturasAltas.php" method="post">
+    <h1>Facturas Ventas</h1>
+    <form action="FacturasAltas.php" method="post">
+        <label for="Comprobante">Comprobante:</label>
+        <input type="text" name="Comprobante" id="Comprobante" required><br>
 
-            <input type="hidden" name="id">
+        <label for="Tipo_Comprobante">Tipo Comprobante:</label>
+        <input type="text" name="Tipo_Comprobante" id="Tipo_Comprobante" required><br>
 
-            <label for="">TIPO DE FACTURA</label>
-            <select name="opcion" id="opcion">
-                <option value="fc">COMPRA</option>
-                <option value="fv">VENTA</option>
-            </select>
-            <br>
+        <label for="Numero">Número de Comprobante:</label>
+        <input type="text" name="Numero" id="Numero" required><br>
 
-            <label for="">Comprobante</label>
-            <select name="opcion" id="opcion">
-                <option value="fc">factura</option>
-                <option value="rc">Recibo</option>
-                <option value="nc">Nota de Credito</option>
-                <option value="nd">Nota de debito</option>
-            </select>
-            <input type="text" name="Comprobante" id="" placeholder=""><br>
-            <!-- fact(fc), notac(nc), notad(nc), recibo(rc) -->
+        <label for="Fecha">Fecha:</label>
+        <input type="date" name="Fecha" id="Fecha" required><br>
 
-            <label for="">Tipo Comprobante</label>
-            <select name="opcion" id="opcion">
-                <option value="a">A</option>
-                <option value="b">B</option>
-                <option value="c">C</option>
-            </select>
-            <input type="text" name="Tipo_Comprobante" id="" placeholder=" ">
-            <br>
-            <!-- a, b, c -->
+        <label for="Id_Contribuyente">ID de Contribuyente:</label>
+        <input type="number" name="Id_Contribuyente" id="Id_Contribuyente" required><br>
 
-            <label for="">numero comprobante</label>
-            <input type="text" name="Numero" id="" placeholder=" ">
-            ejemplos: 0001-00014567
-            <br>
-            <!-- limitar 14 caracteres -->
-            <!-- numero comprobante -->
-            <!-- ejemplos -->
-            <!-- 0001-00014567 -->
-            <!-- 0001-00014567-->
-            
-            <label for="">Fecha</label>
-            <input type="date" name="Fecha" id="" placeholder=" "><br>
+        <label for="Neto_21">Neto 21%:</label>
+        <input type="number" name="Neto_21" id="Neto_21" step="0.01" required><br>
 
-            <label for="">Contribuyente</label>
-            <select name="opcion" id="opcion">
-                <option value="cuil">CUIL</option>
-                <option value="cuit">CUIT</option>
-            </select>
-            <input type="number" name="Id_Contribuyente" id="" placeholder=" "><br>
-            <!-- mostrar cuil Contribuyente -->
-            <!-- select consulta cuil/t db -->
+        <label for="Neto_10_5">Neto 10,5%:</label>
+        <input type="number" name="Neto_10" id="Neto_10" step="0.01" required><br>
 
-            <label for="">Neto 21%</label>
-            <input type="number" name="Neto_21" id="" placeholder=" "><br>
+        <label for="Neto_27">Neto 27%:</label>
+        <input type="number" name="Neto_27" id="Neto_27" step="0.01" required><br>
 
-            <label for="">Neto 10,5%</label>
-            <input type="number" name="Neto_10" id="" placeholder=" "><br>
+        <label for="Iva_21">IVA 21%:</label>
+        <input type="number" name="Iva_21" id="Iva_21" step="0.01" required><br>
 
-            <label for="">Neto 27%</label>
-            <input type="number" name="Neto_27" id="" placeholder=" "><br>
+        <label for="Iva_10_5">IVA 10,5%:</label>
+        <input type="number" name="Iva_10" id="Iva_10" step="0.01" required><br>
 
-            <label for="">IVA 21%</label>
-            <input type="number" name="Iva_21" id="" placeholder=" "><br>
+        <label for="Iva_27">IVA 27%:</label>
+        <input type="number" name="Iva_27" id="Iva_27" step="0.01" required><br>
 
-            <label for="">IVA 10,5%</label>
-            <input type="number" name="Iva_10" id="" placeholder=" "><br>
+        <label for="Exento">Exento:</label>
+        <input type="number" name="Exento" id="Exento" step="0.01" required><br>
 
-            <label for="">IVA 27%</label>
-            <input type="number" name="Iva_27" id="" placeholder=" "><br>
+        <label for="No_Gravado">No Gravado:</label>
+        <input type="number" name="No_Gravado" id="No_Gravado" step="0.01" required><br>
 
-            <label for="">Exento</label>
-            <input type="number" name="Exento" id="" placeholder=" "><br>
+        <label for="Impuestos">Impuestos:</label>
+        <input type="number" name="Impuestos" id="Impuestos" step="0.01" required><br>
 
-            <label for="">No Gravado</label>
-            <input type="number" name="No_Grabado" id="" placeholder=" "><br>
+        <label for="Percepciones">Percepciones:</label>
+        <input type="number" name="Percepciones" id="Percepciones" step="0.01" required><br>
 
-            <label for="">Impuestos</label>
-            <input type="number" name="Impuestos" id="" placeholder=" "><br>
+        <label for="Retenciones">Retenciones:</label>
+        <input type="number" name="Retenciones" id="Retenciones" step="0.01" required><br>
 
-            <label for="">Percepciones</label>
-            <input type="number" name="Percepciones" id="" placeholder=" "><br>
+        <label for="Gastos">Gastos:</label>
+        <input type="number" name="Gastos" id="Gastos" step="0.01" required><br>
 
-            <label for="">Retenciones</label>
-            <input type="number" name="Retenciones" id="" placeholder=" "><br>
+        <label for="Total">Total:</label>
+        <input type="number" name="Total" id="Total" step="0.01" required><br>
 
-            <label for="">Gastos</label>
-            <input type="number" name="Gastos" id="" placeholder=" "><br>
+        <label for="Periodo">Período (mes/año):</label>
+        <input type="text" name="Periodo" id="Periodo" required><br>
 
-            <label for="">Total</label>
-            <input type="number" name="Total" id="" placeholder=" "><br>
+        <label for="Signo">Signo (1 o -1):</label>
+        <input type="number" name="Signo" id="Signo" required><br>
 
-            <label for="">Periodo</label>
-            <input type="number"" name=" Peiodo" id="" placeholder="ingrese mes/año"><br>
-            <!-- periodo por fecha -->
-            <!-- mes/ año -->
+        <label for="id_usuario">ID de Usuario:</label>
+        <input type="number" name="id_usuario" id="id_usuario" required><br>
 
-            <!-- <label for="">Signo</label> -->
-            <input type="hidden" name="Signo" id="">
-            <!-- Validar en JS -->
-
-            <input type="submit" name="submit" value="Enviar">
-        </form>
-    </div>
+        <input type="submit" name="submit" value="Insertar">
+    </form>
 </body>
-
 </html>
